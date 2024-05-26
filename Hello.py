99293d29e00b8362dda610755d6b3b4d2e5a177c
sarthakguptamsfe <sarthakguptamsfe@gmail.com>
2024-05-25 22:10:13 -0700

    fgfrg

--- Hello.py
+++ Hello.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
-import scikit-learn
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
